5a88f6ccd82bb2f638d6d23295a72e4fe709a32a
Dillon Chapuli <148895731+dillonpachuli@users.noreply.github.com>
2024-11-25 17:34:29 +0000

    11.25.24

--- Main.java
+++ Main.java
@@ -1,3 +1,6 @@
-boolean[][] schedule = new boolean[8][60];
-AppointmentBook a = new AppointmentBook(schedule);
-for (int i = 10; i < 15; i++) schedule[1][i] = true;
+public static void main(String[] args)
+{
+    boolean[][] schedule = new boolean[8][60];
+    AppointmentBook a = new AppointmentBook(schedule);
+    for (int i = 10; i < 15; i++) schedule[1][i] = true;
+}
